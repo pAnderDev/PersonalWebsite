69b7839a086f58fb32c94253f21e42b74db5cf16
Novan01 <patrick@casaanderson.net>
2023-07-26 22:55:39 -0500

    modified:   quarkus-personal-website/src/main/java/org/web/HomeResource.java 	deleted:    quarkus-personal-website/src/test/react/App.test.js 	deleted:    quarkus-personal-website/src/test/react/setupTests.js

--- quarkus-personal-website/src/main/java/org/web/HomeResource.java
+++ quarkus-personal-website/src/main/java/org/web/HomeResource.java
@@ -7,7 +7,7 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/home")
+@Path("")
 public class HomeResource {
     @GET
     @Produces(MediaType.TEXT_HTML)
