fa09da1871a53b386551cb79139957ec99388dd6
Novan01 <patrick@casaanderson.net>
2023-07-30 23:44:53 -0500

    new file:   .vscode/settings.json 	modified:   quarkus-personal-website/pom.xml 	modified:   quarkus-personal-website/src/main/java/org/web/DatabaseResource.java 	modified:   quarkus-personal-website/src/main/resources/application.properties

--- quarkus-personal-website/src/main/java/org/web/DatabaseResource.java
+++ quarkus-personal-website/src/main/java/org/web/DatabaseResource.java
@@ -2,11 +2,27 @@ package org.web;
 
 import java.io.InputStream;
 
+import javax.swing.text.Document;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+
 public class DatabaseResource {
-    
+    @Inject MongoClient mongoClient;
+
+    public Document getCVData() {
+        MongoDatabase db = mongoClient.getDatabase("personal-website-db");
+        MongoCollection<Document> collection = db.getCollection("cv-data");
+        Document cvData = collection.find().first();
+        return cvData;
+    }
 }
