d5963cb887bb3364a89417c07d5dc35365ff8c58
Novan01 <patrick@casaanderson.net>
2023-07-30 23:18:19 -0500

    modified:   quarkus-personal-website/pom.xml 	new file:   quarkus-personal-website/src/main/java/org/web/DatabaseResource.java

---
+++ quarkus-personal-website/src/main/java/org/web/DatabaseResource.java
@@ -0,0 +1,12 @@
+package org.web;
+
+import java.io.InputStream;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+public class DatabaseResource {
+    
+}
