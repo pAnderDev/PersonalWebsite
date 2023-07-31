4940ae10a41a10213e743f0af0b59bd4887c6756
Novan01 <patrick@casaanderson.net>
2023-07-31 15:05:02 -0500

    modified:   quarkus-personal-website/src/main/resources/META-INF/resources/javascript/activeSection.js 	modified:   quarkus-personal-website/src/main/resources/META-INF/resources/javascript/topButton.js 	modified:   quarkus-personal-website/src/main/resources/META-INF/resources/pages/about.html 	modified:   quarkus-personal-website/src/main/resources/META-INF/resources/pages/index.html 	modified:   quarkus-personal-website/src/main/resources/META-INF/resources/pages/work_history.html 	modified:   quarkus-personal-website/src/main/resources/META-INF/resources/style/index.css

--- quarkus-personal-website/src/main/resources/META-INF/resources/javascript/activeSection.js
+++ quarkus-personal-website/src/main/resources/META-INF/resources/javascript/activeSection.js
@@ -1,73 +1,15 @@
+let sidebarLinks = document.querySelectorAll(".navbar a");
 
-// const sections = document.querySelectorAll("section");
-// const sidebarLinks = document.querySelectorAll(".sidebar a");
-// let mainSection = document.querySelector(".main-content");
+function removeActionClassFromLinks() {
+    sidebarLinks.forEach((link) => {
+        link.classList.remove("active");
+    });
+}
 
-// function setActiveSection(sectionId) {
-//     sections.forEach((section) => {
-//         section.classList.toggle("active", section.id === sectionId);
-//     });
-
-//     sidebarLinks.forEach((link) => {
-//         link.classList.toggle("active", link.getAttribute("href") === `#${sectionId}`);
-//     });
-// }
-
-// document.addEventListener("scroll", () => {
-//     const scrollPosition = mainSection.scrollTop;
-//     let activeSection = "about"; //default active section view
-//     let maxVisible = 0;
-
-
-//     sections.forEach((section) => {
-//         const sectionTop = section.offsetTop - 10; //adjust based on layout
-//         const sectionHeight = section.clientHeight;
-//         const sectionId = section.id;
-//         if(scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
-//             activeSection = sectionId;
-//         }
-//         if(sectionTop > maxVisible && sectionTop < scrollPosition) {
-//             maxVisible = sectionTop;
-//         }
-    
-//     });
-
-//     setActiveSection(activeSection);
-
-//     if(scrollPosition < maxVisible) {
-//         setActiveSection("about");
-//     }
-// });
-
-
-// const sections = document.querySelectorAll("section");
-// const sidebarLinks = document.querySelectorAll(".sidebar a");
-
-// function setActiveSection(sectionId) {
-//   sections.forEach((section) => {
-//     section.classList.toggle("active", section.id === sectionId);
-//   });
-
-//   sidebarLinks.forEach((link) => {
-//     link.classList.toggle("active", link.getAttribute("href") === `#${sectionId}`);
-//   });
-// }
-
-// const observerOptions = {
-//   root: document.querySelector(".main-content"),
-//   threshold: 0.6, // Adjust the threshold as needed
-// };
-
-// const observer = new IntersectionObserver((entries) => {
-//   entries.forEach((entry) => {
-//     if (entry.isIntersecting) {
-//       const sectionId = entry.target.id;
-//       setActiveSection(sectionId);
-//     }
-//   });
-// }, observerOptions);
-
-// sections.forEach((section) => {
-//   observer.observe(section);
-// });
-
+sidebarLinks.forEach((link) => {
+    link.addEventListener("click", (event) => {
+        event.preventDefault();
+        removeActionClassFromLinks();
+        event.target.classList.add("active");
+    });
+});
--- quarkus-personal-website/src/main/resources/META-INF/resources/javascript/topButton.js
+++ quarkus-personal-website/src/main/resources/META-INF/resources/javascript/topButton.js
@@ -1,32 +1,39 @@
-let topButton = document.getElementById("topbttn");
-let main = document.querySelector(".main-content");
-//When the user scrolls down 20px from the top of the document show the button
-main.onscroll = function() {scrollFunction()};
+document.addEventListener("DOMContentLoaded", function () {
 
-function scrollFunction() {
+    let topButton = document.getElementById("topbttn");
+    let main = document.querySelector(".main-content");
 
-    const scrollHeight = main.scrollHeight;
-    const scrollTop = main.scrollTop;
-    const clientHeight = main.clientHeight;
+    function scrollFunction() {
 
-    if(scrollTop + clientHeight >= scrollHeight - 25 || main.scrollTop > scrollHeight - 25) {
-        topButton.style.display = "block";
-        topButton.classList.add("show");
-        
-    } 
-    else {
-        topButton.classList.remove("show");
-        topButton.style.display = "none";
+        print(`scrollHeight: ${scrollHeight}, scrollTop: ${scrollTop}, clientHeight: ${clientHeight}`);  // Log the values to see if they are correct
+        const scrollHeight = main.scrollHeight;
+        const scrollTop = main.scrollTop;
+        const clientHeight = main.clientHeight;
+
+        if (scrollTop + clientHeight >= scrollHeight - 25 || main.scrollTop > scrollHeight - 25) {
+            topButton.style.display = "block";
+            topButton.classList.add("show");
+
+        }
+        else {
+            topButton.classList.remove("show");
+            topButton.style.display = "none";
+        }
     }
-}
 
 
-function topFunction() {
-    main.scrollTop = 0; //For Safari
-    main.scrollTop = 0; //for all other browsers
-}
+    function topFunction() {
+        main.scrollTop = 0; //For Safari
+        main.scrollTop = 0; //for all other browsers
+    }
 
-main.addEventListener("scroll", scrollFunction);
+    main.addEventListener("scroll", scrollFunction);
 
-// Add event listener for the top button click
-topButton.addEventListener("click", topFunction);
+    // Add event listener for the top button click
+    if(topButton) {
+        topButton.addEventListener("click", topFunction);
+    }
+    else {
+        console.log("Element with id 'topbttn' was not found");
+    }
+});
