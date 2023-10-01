9cdc9e9babf79ff2aedb86d94428ba77001f8a44
Novan01 <patrick@casaanderson.net>
2023-10-01 00:17:57 -0500

    modified:   quarkus-personal-website/pom.xml 	new file:   quarkus-personal-website/src/main/java/org/web/CVData.java 	modified:   quarkus-personal-website/src/main/java/org/web/DatabseResource.java 	new file:   quarkus-personal-website/src/main/resources/META-INF/resources/javascript/databaseGrab.js 	modified:   quarkus-personal-website/src/main/resources/META-INF/resources/pages/index.html 	modified:   quarkus-personal-website/src/main/resources/META-INF/resources/style/index.css

--- quarkus-personal-website/src/main/java/org/web/DatabseResource.java
+++ quarkus-personal-website/src/main/java/org/web/DatabseResource.java
@@ -1,23 +1,29 @@
 package org.web;
-
 import jakarta.inject.Inject;
-
+import java.util.List;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 public class DatabseResource {
-    @Path("/cv")
+    @Path("/cv-data")
     public class CVResource {
 
         @Inject
         DatabaseService dbService;
+
+      
 
         @GET
         @Produces(MediaType.APPLICATION_JSON)
         public String getCV() {
             return dbService.getCVData().toJson();
         }
+        
+       
     }
+
+   
+
 }
