50d57f041fad7c4c3b5ba7e00035ebc59e827194
Novan01 <patrick@casaanderson.net>
2023-11-12 23:10:23 -0600

    deleted:    quarkus-personal-website/src/main/java/org/web/CVData.java 	deleted:    quarkus-personal-website/src/main/java/org/web/DatabaseService.java 	deleted:    quarkus-personal-website/src/main/java/org/web/DatabseResource.java 	deleted:    quarkus-personal-website/src/main/java/org/web/GreetingResource.java 	renamed:    quarkus-personal-website/src/main/resources/META-INF/resources/pages/index.html -> quarkus-personal-website/src/main/resources/META-INF/resources/index.html 	renamed:    quarkus-personal-website/src/main/resources/META-INF/resources/pages/about.html -> quarkus-personal-website/src/main/resources/META-INF/resources/old/pages/old/about.html 	renamed:    quarkus-personal-website/src/main/resources/META-INF/resources/pages/education.html -> quarkus-personal-website/src/main/resources/META-INF/resources/old/pages/old/education.html 	renamed:    quarkus-personal-website/src/main/resources/META-INF/resources/pages/oldindex.html -> quarkus-personal-website/src/main/resources/META-INF/resources/old/pages/old/oldindex.html 	renamed:    quarkus-personal-website/src/main/resources/META-INF/resources/pages/projects.html -> quarkus-personal-website/src/main/resources/META-INF/resources/old/pages/old/projects.html 	renamed:    quarkus-personal-website/src/main/resources/META-INF/resources/pages/skills.html -> quarkus-personal-website/src/main/resources/META-INF/resources/old/pages/old/skills.html 	renamed:    quarkus-personal-website/src/main/resources/META-INF/resources/pages/work_history.html -> quarkus-personal-website/src/main/resources/META-INF/resources/old/pages/old/work_history.html 	renamed:    quarkus-personal-website/src/main/resources/META-INF/resources/javascript/databaseGrab.js -> quarkus-personal-website/src/main/resources/META-INF/resources/old/scripts/old/databaseGrab.js 	renamed:    quarkus-personal-website/src/main/resources/META-INF/resources/javascript/pageSelector.js -> quarkus-personal-website/src/main/resources/META-INF/resources/old/scripts/old/pageSelector.js 	renamed:    quarkus-personal-website/src/main/resources/META-INF/resources/javascript/topButton.js -> quarkus-personal-website/src/main/resources/META-INF/resources/old/scripts/old/topButton.js 	renamed:    quarkus-personal-website/src/main/resources/META-INF/resources/style/index.css -> quarkus-personal-website/src/main/resources/META-INF/resources/old/style/old/index.css 	deleted:    quarkus-personal-website/src/main/resources/META-INF/resources/pages/cv.html 	renamed:    quarkus-personal-website/src/main/resources/META-INF/resources/javascript/activeSection.js -> quarkus-personal-website/src/main/resources/META-INF/resources/scripts.js 	renamed:    quarkus-personal-website/src/main/resources/META-INF/resources/style/style.css -> quarkus-personal-website/src/main/resources/META-INF/resources/style.css

--- quarkus-personal-website/src/main/resources/META-INF/resources/scripts.js
+++ quarkus-personal-website/src/main/resources/META-INF/resources/scripts.js
@@ -1,4 +1,4 @@
-function scrollToElement(elementSelector, instance = 0) {
+function scrollToElement(elementSelector, instance) {
     // Select all elements that match the given selector
     const elements = document.querySelectorAll(elementSelector);
     // Check if there are elements matching the selector and if the requested instance exists
@@ -13,14 +13,14 @@ const link2 = document.getElementById("link2");
 const link3 = document.getElementById("link3");
 
 link1.addEventListener('click', () => {
-    scrollToElement('.header');
+    scrollToElement('.header', 1);
 });
 
 link2.addEventListener('click', () => {
     // Scroll to the second element with "header" class
-    scrollToElement('.header', 1);
+    scrollToElement('.header', 2);
 });
 
 link3.addEventListener('click', () => {
-    scrollToElement('.header', 2);
+    scrollToElement('.header', 3);
 });
