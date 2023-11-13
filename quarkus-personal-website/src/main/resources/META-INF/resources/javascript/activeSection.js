645e272c7eb01ee61086154a322d6bc1ebf097ab
Novan01 <patrick@casaanderson.net>
2023-11-12 22:34:49 -0600

    modified:   quarkus-personal-website/src/main/resources/META-INF/resources/javascript/activeSection.js 	modified:   quarkus-personal-website/src/main/resources/META-INF/resources/pages/index.html 	modified:   quarkus-personal-website/src/main/resources/META-INF/resources/style/style.css

--- quarkus-personal-website/src/main/resources/META-INF/resources/javascript/activeSection.js
+++ quarkus-personal-website/src/main/resources/META-INF/resources/javascript/activeSection.js
@@ -1,15 +1,26 @@
-let sidebarLinks = document.querySelectorAll(".navbar a");
-
-function removeActionClassFromLinks() {
-    sidebarLinks.forEach((link) => {
-        link.classList.remove("active");
-    });
+function scrollToElement(elementSelector, instance = 0) {
+    // Select all elements that match the given selector
+    const elements = document.querySelectorAll(elementSelector);
+    // Check if there are elements matching the selector and if the requested instance exists
+    if (elements.length > instance) {
+        // Scroll to the specified instance of the element
+        elements[instance].scrollIntoView({ behavior: 'smooth' });
+    }
 }
 
-sidebarLinks.forEach((link) => {
-    link.addEventListener("click", (event) => {
-        event.preventDefault();
-        removeActionClassFromLinks();
-        event.target.classList.add("active");
-    });
+const link1 = document.getElementById("link1");
+const link2 = document.getElementById("link2");
+const link3 = document.getElementById("link3");
+
+link1.addEventListener('click', () => {
+    scrollToElement('.header');
+});
+
+link2.addEventListener('click', () => {
+    // Scroll to the second element with "header" class
+    scrollToElement('.header', 1);
+});
+
+link3.addEventListener('click', () => {
+    scrollToElement('.header', 2);
 });
